939529d6d96f90aff829a297639ae3ffbcf7d088
unknown <livistteddy@gmail.com>
2021-05-11 18:54:46 +0100

    Add multiply method to class

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -10,4 +10,16 @@ class Calculator
   def subtract(a, b)
     a - b
   end
+
+  def multiply(*args)
+    init_num = args[0]
+    for num in args do
+      if args.index(num) == 0
+        next
+      else
+        init_num *= num
+      end
+    end
+    init_num
+  end
 end
