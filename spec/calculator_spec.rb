5a096de053137fb7d1e284f3b451a66470fc2cd4
unknown <livistteddy@gmail.com>
2021-05-11 18:46:00 +0100

    Create add method test for more than 2 numbers

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,10 +1,14 @@
 require_relative '../lib/calculator.rb'
 
 describe Calculator do
-  describe "#add" do
-    it "returns the sum of two numbers" do
-      calculator = Calculator.new
+  calculator = Calculator.new
+  context "#add" do
+    example "returns the sum of two numbers" do
       expect(calculator.add(5, 2)).to eql(7)
+    end
+
+    example "returns the sum of more than two numbers" do
+      expect(calculator.add(1, 2, 3)).to eql(6)
     end
   end
 end
